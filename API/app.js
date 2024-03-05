31dd3ed8b1b8a3bdfdc67abe3f1643045c025fb5
ak <ak8893893@yahoo.com.tw>
2024-03-05 15:31:47 +0800

    3-4

--- API/app.js
+++ API/app.js
@@ -1,15 +1,22 @@
-// 引入 express module
-var express = require('express');
+// 引入 chalk 模組 來改變字體顏色
+import chalk from 'chalk';
 
-// 產生一個 express instance - app
-var app = express();
+// 引入 express 模組
+import express from 'express';
 
-// Express 的重點所在! 稍後解釋
-app.get('/', function(req, res){
+// 產生一個 express 實例 - app
+const app = express();
+
+// Express 的核心功能! 稍後解釋
+app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-// 讓 express server 跑在 port 3000
-app.listen(3000, function() {
-    console.log("Listening on port 3000");
+app.post('/123', (req, res) => {
+  res.send('Hello World AK');
+});
+
+// 讓 express 服務器運行在 port 3001
+app.listen(3001, () => {
+    console.log(`Listening on port ${chalk.green('3001')} ${chalk.blue("http://localhost:3001/")}`);
 });
